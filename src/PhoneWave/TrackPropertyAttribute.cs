1acd2582ef8530b9533e1d0eba54aac8fa029f3e
Nikita Tsukanov <keks9n@gmail.com>
2022-06-01 20:27:51 +0300

    AllowMultiple

--- src/PhoneWave/TrackPropertyAttribute.cs
+++ src/PhoneWave/TrackPropertyAttribute.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace PhoneWave;
 
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
 public class TrackPropertyAttribute : Attribute
 {
     public TrackPropertyAttribute(Type type, string name)
