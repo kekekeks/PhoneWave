e4920680319aa04c855f37e166bc3eb490e1fdfb
Thehx <thehdotx@gmail.com>
2022-11-02 20:54:42 +0600

    Added transaction-aware collection and dictionary support

---
+++ src/PhoneWave/PhoneWaveCollection.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+
+namespace PhoneWave;
+
+public class PhoneWaveCollection<T> : ObservableCollection<T>
+{
+    class PhoneWaveCollectionChangeAggregator : TransactionChange
+    {
+        private readonly List<(bool isRemove, int index, T item)> _changes = new();
+
+        private readonly PhoneWaveCollection<T> _target;
+
+        public PhoneWaveCollectionChangeAggregator(PhoneWaveCollection<T> target)
+        {
+            _target = target;
+        }
+
+        public void AddItem(T item, int index) => _changes.Add((false, index, item));
+        public void RemoveItem(T item, int index) => _changes.Add((true, index, item));
+
+        public override void Rollback()
+        {
+            foreach (var (isRemove, index, item) in Enumerable.Reverse(_changes))
+            {
+                if (isRemove) _target.InsertItemNoTransaction(index, item);
+                else _target.RemoveItemNoTransaction(index);
+            }
+        }
+        public override void RollForward()
+        {
+            foreach (var (isRemove, index, item) in _changes)
+            {
+                if (!isRemove) _target.InsertItemNoTransaction(index, item);
+                else _target.RemoveItemNoTransaction(index);
+            }
+        }
+    }
+    static void UpdateCollection(PhoneWaveContext context, PhoneWaveCollection<T> target, bool isRemoval, int index, T item)
+    {
+        void MakeChange(PhoneWaveCollectionChangeAggregator<T> x)
+        {
+            if (isRemoval) x.RemoveItem(item, index);
+            else x.AddItem(item, index);
+        }
+
+        if (context.TryGetChanges<PhoneWaveCollectionChangeAggregator<T>>(target, out var x))
+            MakeChange(x);
+        else
+        {
+            var da = new PhoneWaveCollectionChangeAggregator<T>(target);
+            MakeChange(da);
+            context.AddChange(target, da);
+        }
+    }
+
+    public PhoneWaveContext Context { get; }
+
+    public PhoneWaveCollection(PhoneWaveContext context)
+    {
+        Context = context;
+    }
+    void InsertItemNoTransaction(int index, T item) => base.InsertItem(index, item);
+    void RemoveItemNoTransaction(int index) => base.RemoveItem(index);
+    protected override void ClearItems()
+    {
+        for (int i = Count - 1; i >= 0; i--)
+        {
+            UpdateCollection(Context, this, true, i, this[i]);
+        }
+        base.ClearItems();
+    }
+    protected override void InsertItem(int index, T item)
+    {
+        UpdateCollection(Context, this, false, index, item);
+        base.InsertItem(index, item);
+    }
+    protected override void MoveItem(int oldIndex, int newIndex)
+    {
+        var item = this[oldIndex];
+        UpdateCollection(Context, this, true, oldIndex, item);
+        UpdateCollection(Context, this, false, newIndex, item);
+
+        base.MoveItem(oldIndex, newIndex);
+    }
+    protected override void RemoveItem(int index)
+    {
+        UpdateCollection(Context, this, true, index, this[index]);
+        base.RemoveItem(index);
+    }
+    protected override void SetItem(int index, T item)
+    {
+        UpdateCollection(Context, this, true, index, this[index]);
+        UpdateCollection(Context, this, false, index, item);
+        base.SetItem(index, item);
+    }
+}
---
+++ src/PhoneWave/PhoneWaveDictionary.cs
@@ -0,0 +1,220 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Collections;
+using System.Linq;
+
+namespace PhoneWave;
+
+public class PhoneWaveDictionary<TKey, TValue> : IDictionary<TKey, TValue>, INotifyCollectionChanged where TKey : notnull
+{
+    class PhoneWaveDictionaryChangeAggregator : TransactionChange
+    {
+        class State
+        {
+            public TValue? OldValue { get; set; }
+            public TValue? NewValue { get; set; }
+            public bool HasOldValue { get; set; }
+            public bool HasNewValue { get; set; }
+
+            public State(bool hasOldValue, TValue? oldValue, bool hasNewValue, TValue? newValue)
+            {
+                OldValue = oldValue;
+                NewValue = newValue;
+                HasOldValue = hasOldValue;
+                HasNewValue = hasNewValue;
+            }
+        }
+        private readonly Dictionary<TKey, State> _states = new();
+        private readonly PhoneWaveDictionary<TKey, TValue> _target;
+        public PhoneWaveDictionaryChangeAggregator(PhoneWaveDictionary<TKey, TValue> target) => _target = target;
+
+        State GetState(TKey key, bool hasOldValue, TValue? oldValue)
+        {
+            if (_states.TryGetValue(key, out var diff))
+                return diff;
+            return _states[key] = new State(hasOldValue, oldValue, false, default);
+        }
+
+        public void AddAdd(TKey key, TValue value)
+        {
+            var state = GetState(key, false, default);
+            state.HasNewValue = true;
+            state.NewValue = value;
+        }
+        public void AddRemove(TKey key, TValue value)
+        {
+            var state = GetState(key, true, value);
+            state.HasNewValue = false;
+            state.NewValue = default;
+        }
+        public void AddChange(TKey key, TValue oldValue, TValue newValue)
+        {
+            var state = GetState(key, true, oldValue);
+            state.HasNewValue = true;
+            state.NewValue = newValue;
+        }
+
+        public override void Rollback()
+        {
+            foreach (var s in _states)
+            {
+                if (s.Value.HasOldValue) _target.SetNoTransaction(s.Key, s.Value.OldValue!);
+                else if (!s.Value.HasNewValue) _target.RemoveNoTransaction(s.Key);
+            }
+        }
+
+        public override void RollForward()
+        {
+            foreach (var s in _states)
+            {
+                if (s.Value.HasNewValue) _target.SetNoTransaction(s.Key, s.Value.NewValue!);
+                else if (!s.Value.HasOldValue) _target.RemoveNoTransaction(s.Key);
+            }
+        }
+    }
+
+    static void AddToDictionary(PhoneWaveContext context, PhoneWaveDictionary<TKey, TValue> target,
+        TKey key, TValue value)
+    {
+        if (context.TryGetChanges<PhoneWaveDictionaryChangeAggregator>(key, out var ch))
+        {
+            ch.AddAdd(key, value);
+        }
+        else
+        {
+            var newChange = new PhoneWaveDictionaryChangeAggregator(target);
+            newChange.AddAdd(key, value);
+            context.AddChange(key, newChange);
+        }
+    }
+    static void RemoveFromDictionary(PhoneWaveContext context, PhoneWaveDictionary<TKey, TValue> target,
+        TKey key, TValue value)
+    {
+        if (context.TryGetChanges<PhoneWaveDictionaryChangeAggregator>(key, out var ch))
+        {
+            ch.AddRemove(key, value);
+        }
+        else
+        {
+            var newChange = new PhoneWaveDictionaryChangeAggregator(target);
+            newChange.AddRemove(key, value);
+            context.AddChange(key, newChange);
+        }
+    }
+    static void UpdateDictionary(PhoneWaveContext context, PhoneWaveDictionary<TKey, TValue> target,
+        TKey key, TValue oldValue, TValue newValue)
+    {
+        if (context.TryGetChanges<PhoneWaveDictionaryChangeAggregator>(key, out var ch))
+        {
+            ch.AddChange(key, oldValue, newValue);
+        }
+        else
+        {
+            var newChange = new PhoneWaveDictionaryChangeAggregator(target);
+            newChange.AddChange(key, oldValue, newValue);
+            context.AddChange(key, newChange);
+        }
+    }
+
+    /// <summary>
+    /// This is only supposed to be used from Rollback/RollForward.
+    /// Adds or replaces the value for given key.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    void SetNoTransaction(TKey key, TValue value) => _dictionary[key] = value;
+    /// <summary>
+    /// This is only supposed to be used from Rollback/RollForward.
+    /// Removes the entry with given key. 
+    /// </summary>
+    /// <param name="key"></param>
+    void RemoveNoTransaction(TKey key) => _dictionary.Remove(key);
+
+    public PhoneWaveContext Context { get; }
+
+    public PhoneWaveDictionary(PhoneWaveContext context) => Context = context;
+
+    public event NotifyCollectionChangedEventHandler CollectionChanged
+    {
+        add => _dictionary.CollectionChanged += value;
+        remove => _dictionary.CollectionChanged -= value;
+    }
+
+    private readonly AvaloniaDictionary<TKey, TValue> _dictionary = new();
+
+    public TValue this[TKey key]
+    {
+        get => _dictionary[key];
+        set
+        {
+            if (_dictionary.ContainsKey(key))
+            {
+                var oldValue = _dictionary[key];
+                _dictionary[key] = value;
+                UpdateDictionary(Context, this, key, oldValue, value);
+            }
+            else
+            {
+                _dictionary[key] = value;
+                AddToDictionary(Context, this, key, value);
+            }
+        }
+    }
+
+    public ICollection<TKey> Keys => _dictionary.Keys;
+
+    public ICollection<TValue> Values => _dictionary.Values;
+
+    public int Count => _dictionary.Count;
+
+    public bool IsReadOnly => false;
+
+    public void Add(TKey key, TValue value)
+    {
+        _dictionary.Add(key, value);
+        AddToDictionary(Context, this, key, value);
+    }
+
+    public void Add(KeyValuePair<TKey, TValue> item) => Add(item.Key, item.Value);
+
+    public void Clear()
+    {
+        var kvps = this.ToList();
+        _dictionary.Clear();
+        foreach (var item in kvps)
+            RemoveFromDictionary(Context, this, item.Key, item.Value);
+    }
+    public bool Remove(TKey key)
+    {
+        _dictionary.TryGetValue(key, out var old);
+        var result = _dictionary.Remove(key);
+        if (result) RemoveFromDictionary(Context, this, key, old);
+        return result;
+    }
+
+    public bool Contains(KeyValuePair<TKey, TValue> item) => _dictionary.Contains(item);
+
+    public bool ContainsKey(TKey key) => _dictionary.ContainsKey(key);
+
+    void ICollection<KeyValuePair<TKey, TValue>>.CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) => ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).CopyTo(array, arrayIndex);
+
+    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => _dictionary.GetEnumerator();
+
+    public bool Remove(KeyValuePair<TKey, TValue> item)
+    {
+        if (((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).Remove(item))
+        {
+            RemoveFromDictionary(Context, this, item.Key, item.Value);
+            return true;
+        }
+        return false;
+    }
+
+    public bool TryGetValue(TKey key,
+#if NET6_0_OR_GREATER
+        [MaybeNullWhen(false)] 
+#endif
+        out TValue value) => _dictionary.TryGetValue(key, out value);
+
+    IEnumerator IEnumerable.GetEnumerator() => _dictionary.GetEnumerator();
+}
